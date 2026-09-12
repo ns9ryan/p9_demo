ec4638dafb4e8071c3f7b67322cb2fe033a1a96a
ryan <ryan@company.com>
2026-09-13 00:01:52 +0800

    1

--- platform-base-demo/rpc/internal/logic/regionservice/get_by_code_logic.go
+++ platform-base-demo/rpc/internal/logic/regionservice/get_by_code_logic.go
@@ -2,11 +2,16 @@ package regionservicelogic
 
 import (
 	"context"
-
-	"oa.98ent.com/p9/platform-base/rpc/internal/svc"
-	"oa.98ent.com/p9/platform-base/rpc/pb/base/region"
+	"strings"
 
 	"github.com/zeromicro/go-zero/core/logx"
+
+	"oa.98ent.com/p9/platform-base/pkg/i18nkey"
+	"oa.98ent.com/p9/platform-base/pkg/rpc/grpcerror"
+	entregion "oa.98ent.com/p9/platform-base/rpc/ent/region"
+	"oa.98ent.com/p9/platform-base/rpc/internal/enterror"
+	"oa.98ent.com/p9/platform-base/rpc/internal/svc"
+	"oa.98ent.com/p9/platform-base/rpc/pb/base/region"
 )
 
 type GetByCodeLogic struct {
@@ -23,9 +28,26 @@ func NewGetByCodeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetByCo
 	}
 }
 
-// 按编码获取国家地区
+// GetByCode 按编码获取国家地区
 func (l *GetByCodeLogic) GetByCode(in *region.GetRegionByCodeRequest) (*region.GetRegionByCodeResponse, error) {
-	// todo: add your logic here and delete this line
+	// 整理国家地区编码
+	code := strings.ToUpper(strings.TrimSpace(in.Code))
+	if code == "" {
+		return nil, grpcerror.InvalidArgument(i18nkey.ValidationError)
+	}
 
-	return &region.GetRegionByCodeResponse{}, nil
+	// 按编码获取国家地区
+	result, err := l.svcCtx.DB.Region.
+		Query().
+		Where(entregion.CodeEQ(code)).
+		Only(l.ctx)
+	if err != nil {
+		// 转换Ent错误为gRPC错误
+		return nil, enterror.Handle(l.Logger, err)
+	}
+
+	// 返回国家地区信息
+	return &region.GetRegionByCodeResponse{
+		Region: toRegionInfo(result), // 国家地区信息
+	}, nil
 }
